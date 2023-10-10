c83a37252e87998219647c0106c46554a0b715d3
Shant Dashjian <shantgdashjian@gmail.com>
2023-10-10 11:42:17 +0400

    Add styling and Vans UI

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,16 +1,42 @@
 import React from "react"
-import { useEffect } from "react"
+
+/**
+ * {
+    * id: "1", 
+    * name: "Modest Explorer", 
+    * price: 60, 
+    * description: "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!", 
+    * imageUrl: "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png", 
+    * type: "simple"
+ * }
+ */
+
 
 export default function Vans() {
-  useEffect(() => {
+  const [vans, setVans] = React.useState([])
+  React.useEffect(() => {
     fetch("/api/vans")
       .then(res => res.json())
-      .then(data => {
-        console.log(data)
-      })
+      .then(data => setVans(data.vans))
   }, [])
 
+  const vanElements = vans.map(van => (
+    <div key={van.id} className="van-tile">
+      <img src={van.imageUrl} />
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <p>${van.price}<span>/day</span></p>
+      </div>
+      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+    </div>
+  ))
+
   return (
-    <h1>Vans page goes here 🚐</h1>
+    <div className="van-list-container">
+      <h1>Explore our van options</h1>
+      <div className="van-list">
+        {vanElements}
+      </div>
+    </div>
   )
 }
