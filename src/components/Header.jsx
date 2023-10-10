a529adc0547ba1fffe31410f90e8207c0036d95f
Shant Dashjian <shantgdashjian@gmail.com>
2023-10-10 17:25:02 +0400

    Add Header and Layout components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <Link className="site-logo" to="/">#VanLife</Link>
+      <nav>
+        <Link to="/about">About</Link>
+        <Link to="/vans">Vans</Link>
+      </nav>
+    </header>
+  )
+}
