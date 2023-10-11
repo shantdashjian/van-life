dc516628c7d914fc483c5516f83d89994ac68c77
Shant Dashjian <shantgdashjian@gmail.com>
2023-10-11 18:26:41 +0400

    Add host nav and routes

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from "./pages/Vans/Vans"
+import VanDetail from "./pages/Vans/VanDetail"
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
 import Layout from "./components/Layout"
-
-import Dashboard from './pages/Host/Dashboard'
-import Income from './pages/Host/Income'
-import Reviews from './pages/Host/Reviews'
+import HostLayout from "./components/HostLayout"
 
 import "./server"
-import Vans from './pages/Vans/Vans'
-import VanDetail from './pages/Vans/VanDetail'
 
 function App() {
   return (
@@ -22,9 +22,12 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Dashboard />} />
-          <Route path="/host/income" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
+
+          <Route path="/host" element={<HostLayout />}>
+            <Route path="/host" element={<Dashboard />} />
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
