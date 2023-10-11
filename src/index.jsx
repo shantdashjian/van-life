f050839889000507f765a08c89e953b44a67d7fb
Shant Dashjian <shantgdashjian@gmail.com>
2023-10-11 17:50:40 +0400

    Add Layout and Outlet to show navbar

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Vans from './pages/Vans'
-import VanDetail from './pages/VanDetail'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans from "./pages/Vans"
+import VanDetail from "./pages/VanDetail"
+import Layout from "./components/Layout"
 
 import "./server"
 
 function App() {
   return (
     <BrowserRouter>
-      
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/vans' element={<Vans />} />
-        <Route path='/vans/:id' element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
@@ -24,4 +26,4 @@ function App() {
 
 ReactDOM
   .createRoot(document.getElementById('root'))
-  .render(<App />)
+  .render(<App />);
