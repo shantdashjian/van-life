72550cc2f5d63739a2203def2b12cb07910ebc10
Shant Dashjian <shantgdashjian@gmail.com>
2023-10-11 18:03:23 +0400

    Bootstrap Host and its pages

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans from "./pages/Vans"
-import VanDetail from "./pages/VanDetail"
 import Layout from "./components/Layout"
 
+import Dashboard from './pages/Host/Dashboard'
+import Income from './pages/Host/Income'
+import Reviews from './pages/Host/Reviews'
+
 import "./server"
+import Vans from './pages/Vans/Vans'
+import VanDetail from './pages/Vans/VanDetail'
 
 function App() {
   return (
@@ -18,6 +22,9 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<Dashboard />} />
+          <Route path="/host/income" element={<Income />} />
+          <Route path="/host/reviews" element={<Reviews />} />
         </Route>
       </Routes>
     </BrowserRouter>
